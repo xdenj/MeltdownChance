87faac73a6e4cf03d21c20344fb4797c69349c0e
den <dennis@jlnnk.de>
2024-03-29 20:00:13 +0100

    Untrack ignored files

--- obj/Release/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MeltdownChance
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MeltdownChance";
-        public const string PLUGIN_NAME = "MeltdownChance";
-        public const string PLUGIN_VERSION = "2.5.0";
-    }
-}
