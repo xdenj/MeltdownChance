7c49abd6f4906078ca698ba920caf91291d15a23
den <den@jlnnk.de>
2024-04-01 08:33:59 +0200

    Separated configs into their own class

---
+++ Configs/MeltdownChanceConfig.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeltdownChance.Configs
+{
+    public class MeltdownChanceConfig
+    {
+        public static ConfigEntry<int> configChance;
+        public static ConfigEntry<bool> configMessage;
+
+        public MeltdownChanceConfig(ConfigFile cfg)
+        {
+            configChance = cfg.Bind("General", "MeltdownChance", 100, "Chance in percent (0 - 100) at which Meltdowns should occur");
+            configMessage = cfg.Bind("General", "DisplayPopup", true, "Display Meltdown Chance popup when picking up the Apparatus");
+        }
+    }
+}
